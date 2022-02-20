f5af85f7d0e391d85fd8194ba9f50e29e4c5ea61
Saeed Heydari <saeedhpro@gmail.com>
2022-02-20 21:05:14 +0330

    compose

--- repository/init.go
+++ repository/init.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	DBS  dbs
+	DBS dbs
 	//host = "simatebdb:3306"
 	//host     = "localhost:3306"
 	////schema = "simateb"
@@ -30,7 +30,7 @@ func mysqlInit() {
 	username := "admin_simateb"
 	password := "admin_simateb"
 	schema := "admin_simateb"
-	host := "localhost:3306"
+	host := "mysql:3306"
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=true", username, password, host, schema)
 	db, err := sql.Open("mysql", dataSourceName)
 
